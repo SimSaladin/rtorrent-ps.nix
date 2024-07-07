31033c8834b36aa5ec9ba83a9be3176df91e4e37
Samuli Thomasson <samuli.thomasson@pm.me>
2024-07-07 04:34:05 +0200

    remove gcc tune as well in the hopes it solves a certain segfault issue

--- functions.nix
+++ functions.nix
@@ -5,5 +5,5 @@ _lfinal: _lprev:
     ((sys.isx86_64 && sys.gcc ? arch) -> sys.gcc.arch == "x86-64");
 
   makeGenericSystem = sys:
-    if sys.isx86_64 then sys // { gcc = builtins.removeAttrs sys.gcc [ "arch" ]; } else sys;
+    if sys.isx86_64 then sys // { gcc = builtins.removeAttrs sys.gcc [ "arch" "tune" ]; } else sys;
 }
